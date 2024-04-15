c4603be037acfb38f46af7b1114561c495bc3f2d
TeoAntony <teoantonycu123@gmail.com>
2024-04-15 00:44:26 -0500

    avance categoria

--- src/app/services/estado-inscripcion.service.ts
+++ src/app/services/estado-inscripcion.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { EstadoInscripcionModel } from '../models/estado-inscripcion.model';
-import { Observable, Subject, of, pipe, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
